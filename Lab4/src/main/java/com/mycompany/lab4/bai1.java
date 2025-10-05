797425daa14e67ac6946608e5f3fb07cea1a1ca8
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-10-05 22:16:24 +0700

    Lab4_bai1

--- Lab4/src/main/java/com/mycompany/lab4/bai1.java
+++ Lab4/src/main/java/com/mycompany/lab4/bai1.java
@@ -1,16 +1,47 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.lab4;
 
-/**
- *
- * @author ICT
- */
-public class bai1 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class bai1 {
+    public String tenSp;
+    public Double donGia;
+    public Double giamGia;
+
+    public bai1(String tenSp, Double donGia, Double giamGia) {
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+
+    public bai1() {
+    }
+
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ten san pham: ");
+        tenSp = sc.nextLine();
+        System.out.print("Nhap don gia: ");
+        donGia = sc.nextDouble();
+        System.out.print("Nhap giam gia: ");
+        giamGia = sc.nextDouble();
+    }
+
+    public double getThueNhapKhau() {
+        return donGia * 0.1;
+    }
+
+    public void xuat() {
+        System.out.println("Ten san pham: " + tenSp);
+        System.out.println("Don gia: " + donGia);
+        System.out.println("Giam gia: " + giamGia);
+        System.out.println("Thue nhap khau: " + getThueNhapKhau());
+    }
+    
+    
+     public static void main(String[] args) {
+        bai1 sp = new bai1();   
+        sp.nhap();
+        System.out.println("\n--- Thong tin san pham ---");
+        sp.xuat();
     }
 }
