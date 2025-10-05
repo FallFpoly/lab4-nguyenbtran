df6261e28b397ea2ea3121382e7dfe3b335f21b9
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-10-05 22:16:33 +0700

    Lab4_bai2

--- Lab4/src/main/java/com/mycompany/lab4/bai2.java
+++ Lab4/src/main/java/com/mycompany/lab4/bai2.java
@@ -8,9 +8,41 @@ package com.mycompany.lab4;
  *
  * @author ICT
  */
-public class bai2 {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class bai2 {
+    public String tenSp;
+    public double donGia;
+    public double giamGia;
+
+    public bai2() {
+    }
+
+    public bai2(String tenSp, double donGia, double giamGia) {
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+
+    public void nhap() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ten san pham: ");
+        tenSp = sc.nextLine();
+        System.out.print("Nhap don gia: ");
+        donGia = sc.nextDouble();
+        System.out.print("Nhap giam gia: ");
+        giamGia = sc.nextDouble();
+    }
+
+    public double getThueNhapKhau() {
+        return donGia * 0.1; // 10% giá sản phẩm
+    }
+
+    public void xuat() {
+        System.out.println("Ten san pham: " + tenSp);
+        System.out.println("Don gia: " + donGia);
+        System.out.println("Giam gia: " + giamGia);
+        System.out.println("Thue nhap khau: " + getThueNhapKhau());
+        System.out.println("-------------------------------");
     }
 }
