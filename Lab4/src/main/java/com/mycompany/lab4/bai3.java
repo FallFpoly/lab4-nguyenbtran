9ff20bcc235accdcc57d02427bd911e360377ddb
trannpbtb00387-bit <trannpbtb00387@fpt.edu.vn>
2025-10-05 22:16:41 +0700

    Lab4_bai3

--- Lab4/src/main/java/com/mycompany/lab4/bai3.java
+++ Lab4/src/main/java/com/mycompany/lab4/bai3.java
@@ -8,6 +8,33 @@ package com.mycompany.lab4;
  *
  * @author ICT
  */
+
 public class bai3 {
-    
+    public String tenSp;
+    public double donGia;
+    public double giamGia;
+
+
+    public bai3(String tenSp, double donGia, double giamGia) {
+        this.tenSp = tenSp;
+        this.donGia = donGia;
+        this.giamGia = giamGia;
+    }
+
+    public bai3(String tenSp, double donGia) {
+        this(tenSp, donGia, 0);
+    }
+
+    private double getThueNhapKhau() {
+        return donGia * 0.1;
+    }
+
+    public void xuat() {
+        System.out.println("Ten san pham: " + tenSp);
+        System.out.println("Don gia: " + donGia);
+        System.out.println("Giam gia: " + giamGia);
+        System.out.println("Thue nhap khau: " + getThueNhapKhau());
+        System.out.println("-----------------------------");
+    }
 }
+
